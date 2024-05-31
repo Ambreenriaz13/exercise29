6790921d4c71633dc315eee78e17b241f69cd091
Ambreen Riaz <159724062+Ambreenriaz13@users.noreply.github.com>
2024-05-31 23:37:27 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+// creating an Array
+let favourite_fruit = ["Mango", "Orange", "Watermelon"];
+if (favourite_fruit.includes("Mango")) {
+    console.log(" I Really Like Mangos");
+}
+if (favourite_fruit.includes("Orange")) {
+    console.log(" I Really Like Oranges");
+}
+if (favourite_fruit.includes("Apple")) {
+    console.log(" I Really Like Apple");
+}
+if (favourite_fruit.includes("Watermelon")) {
+    console.log(" I Really Like Watermelon");
+}
+if (favourite_fruit.includes("Banana")) {
+    console.log(" I Really Like Bananas");
+}
